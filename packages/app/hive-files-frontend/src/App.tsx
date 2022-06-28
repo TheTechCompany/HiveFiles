85b429840990cc44786d3250cb72edf2bcdfd3bb
Ross Leitch <ross@hexhive.io>
2022-06-28 18:19:02 +1200

    Add cors to files backend

--- packages/app/hive-files-frontend/src/App.tsx
+++ packages/app/hive-files-frontend/src/App.tsx
@@ -8,18 +8,57 @@ import { useEffect } from "react";
 import { BrowserRouter as Router, matchPath, Route, Routes } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache, gql } from "@apollo/client";
 import { Explorer } from "./views/explorer";
+import { createTheme, ThemeProvider } from "@mui/material"
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#72738b'
+    },
+    secondary: {
+      // light: '#a3b579',
+      main: "#87927e"
+    }
+  },
+  components: {
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          borderRadius: '6px',
+          overflow: "hidden"
+        }
+      }
+    },
+    MuiTableHead: {
+      styleOverrides: {
+        root: {
+          background: 'green'
+        }
+      }
+    }
+  }
+  // palette: {
+  //   // primary: {
+  //   //   main: '',
+  //   // },
+  //   // secondary: {
+  //   //   main: ''
+  //   // }
+  // }
+});
 
 export const App = (props: any)  => {
 
   return (
-   
-  <Box background="neutral-2" flex direction="column">
-    <SidebarView  
-      views={[
-        {label: 'Files', icon: <Folder />, path: '/', component: <Explorer />},
-        {label: 'Shared', icon: <Share />, path: '/shared', component: <Box>Shared</Box>}
-      ]} />
+   <ThemeProvider theme={theme}>
+    <Box background="neutral-2" flex direction="column">
+      <SidebarView  
+        views={[
+          {label: 'Files', icon: <Folder />, path: '/', component: <Explorer />},
+          {label: 'Shared', icon: <Share />, path: '/shared', component: <Box>Shared</Box>}
+        ]} />
 
-  </Box>);
+    </Box>
+  </ThemeProvider>
+  );
 }
